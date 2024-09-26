a8e9694e4a66014bf5c3d0d0bca1ab61dd244be7
Alex Ventura <aventura3@huskers.unl.edu>
2024-09-26 12:59:27 -0500

    Added my name and major (LA)

--- hello.c
+++ hello.c
@@ -2,7 +2,9 @@
 #include<stdio.h>
 
 int main(int argc, char **argv) {
-printf( "Christian Olson!\n" );
-printf( "my major is Computer Science.\n");
+    printf( "Christian Olson!\n" );
+    printf( "my major is Computer Science.\n");
 
+    printf("Alex Ventura\n");
+    printf("My major is Computer Engineering\n");
 }
